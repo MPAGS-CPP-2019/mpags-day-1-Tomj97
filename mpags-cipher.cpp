f0a4e313e21495821fd6aa79b59a00148f50de78
Tomj97 <Tom.P.Jones@warwick.ac.uk>
2019-10-11 14:48:27 +0100

    Added a function that contains the solution to example 4 of the C++ course

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 void varManipulation();
 void constVarManipulation();
+void exampleSol4();
 /*
 Main code block, calls functions
 */
 int main()
 {
-  string helloWorld = "Hello World!";
+  
   // varManipulation();
-  constVarManipulation();
-  cout << "This has been created as a string type variable: " << helloWorld << endl;
-
+  //constVarManipulation();
+  exampleSol4();
 }
-
+/*Solution to EX2 of C++ course */
 void varManipulation()
 {
 
@@ -31,7 +31,7 @@ void varManipulation()
   cout<< "This is the created double: " << c << endl;
   cout<< "This is the integer created from the double: " << d << endl;
 }
-
+/*Solution to EX3 of C++ course*/
 void constVarManipulation()
 {
 
@@ -42,5 +42,26 @@ void constVarManipulation()
   cout << "This is a contant double: " << i << endl;
   
   cout << "This is a variable integer: " << k << endl;
+
+}
+
+void exampleSol4()
+{
+
+  const int a{5};
+  const double b{10.5};
+  int product = a*b;
+  double doubDivInt = b/a;
+  int intDivInt = a/a;
+  string newString("LHCb");
+  char newChar = newString[3];
+
+
+  cout << "Product of double and int: " << product << endl;
+  cout << "Double divided by int: " << doubDivInt << endl;
+  cout << "Int divided by int: " << intDivInt << endl;
+  cout << "This is my string: " << newString << endl;
+  cout << "This is the 4th char of my string: " << newString[3] << endl;
+  
 
 }
